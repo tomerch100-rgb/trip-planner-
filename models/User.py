d2a8bf49a6e73c97ae4e9a1a25c7b283214a2368
tomershurany <tomershorany@gmail.com>
2026-06-08 19:11:52 +0300

    Update User.py

--- models/User.py
+++ models/User.py
@@ -1,6 +1,4 @@
-
 from pydantic import BaseModel, Field
-
 # Model for receiving user data - Frontend during registration or login
 class UserCreate(BaseModel):
     username: str = Field(..., min_length=3, description="Username must contain at least 3 characters")
