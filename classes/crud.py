8856cd1cc83be9743f1e44531c895c981a68720f
tomerch100-rgb <tomerch100@gmail.com>
2026-06-09 15:34:41 +0300

    fixed version for models
    finish the models and crud for the router

--- classes/crud.py
+++ classes/crud.py
@@ -33,8 +33,8 @@ def authenticate_user(db: Session, user_login: schemas.UserLogin):
 
 
 def create_trip(db: Session, trip: schemas.TripCreate, user_id: int):
-    new_trip =  models.Trip (
-        destination=trip.destination,
+    new_trip = models.Trip(
+        city_id=trip.city_id, 
         start_date=trip.start_date,
         end_date=trip.end_date,
         user_id=user_id  
@@ -47,3 +47,19 @@ def create_trip(db: Session, trip: schemas.TripCreate, user_id: int):
 
 def get_user_trips(db: Session, user_id: int):
     return db.query(models.Trip).filter(models.Trip.user_id == user_id).all()
+
+
+
+def get_attractions_by_city(db: Session, city_id: int):
+
+    return db.query(models.Attraction).filter(models.Attraction.city_id == city_id).all()
+
+
+
+def get_attractions_by_category(db: Session, city_id: int, category_id: int):
+
+    return db.query(models.Attraction).filter(
+        models.Attraction.city_id == city_id,
+        models.Attraction.category_id == category_id
+    ).all()
+
--- classes/models.py
+++ classes/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime,Date, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime,Date, ForeignKey,Numeric
 from sqlalchemy.sql import func
 from DB.db import Base
 
@@ -15,7 +15,26 @@ class User(Base):
 class Trip(Base):
     __tablename__ = "trips" 
     id = Column(Integer, primary_key=True, index=True)
-    destination = Column(String, index=True)
+    city_id = Column(Integer, ForeignKey("cities.id")) 
     start_date = Column(Date)
     end_date = Column(Date)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+
+
+class AttractionCategory(Base):
+
+    __tablename__ = "attraction_categories"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), unique=True, nullable=False)
+
+
+class Attraction(Base):
+  
+    __tablename__ = "attractions"
+    id = Column(Integer, primary_key=True, index=True)
+    category_id = Column(Integer, ForeignKey("attraction_categories.id"))
+    city_id = Column(Integer, ForeignKey("cities.id")) 
+    name = Column(String(100), nullable=False)
+    address = Column(String(255), nullable=False)
+    default_price = Column(Numeric(10, 2), default=0.00)    
